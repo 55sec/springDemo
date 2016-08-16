5e77718d39d3eae3bf51929c018dc1b6db74f28c
xiayiwei <yiwei.xia@ele.me>
2016-08-16 21:33:56 +0800

    add springmvc web

---
+++ springweb/src/main/java/com/xyw55/springMVC/HelloController.java
@@ -0,0 +1,20 @@
+package com.xyw55.springMVC;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+/**
+ * Created by xiayiwei on 16/8/16.
+ */
+@Controller
+@RequestMapping("/hello")
+public class HelloController {
+    @RequestMapping(method = RequestMethod.GET)
+    public String printHello(ModelMap model) {
+        model.addAttribute("message", "hello spring");
+        return "hello";
+    }
+
+}
